f3497a24fb0fa0128397559c7c0ade95aa1be0b2
Alon Gat <alon@skycure.com>
2016-07-23 23:14:18 +0300

    some addional calls

--- lib/budgetbakers.rb
+++ lib/budgetbakers.rb
@@ -9,12 +9,19 @@ module Budgetbakers
     API_VERSION = '3.0'
 
     # Initialize
-    def initialize
+    def initialize(email)
       self.class.base_uri('https://api.budgetbakers.com/api/v1')
       @headers = {
           'X-Token' => ENV['BB_TOKEN'],
-          'Content-Type' => 'application/json'
+          'Content-Type' => 'application/json',
+          'X-User' => email
       }
+      @categories = {}
+      @accounts = {}
+      @currencies = {}
+      list_accounts
+      list_currencies
+      list_categories
     end
 
     def check_email(email)
@@ -22,21 +29,78 @@ module Budgetbakers
       response.code == 200
     end
 
-    def list_accounts(email)
-      response = get('/accounts', additional_headers: { 'X-User' => email })
-      ap response
+    def list_accounts
+      res = get('/accounts')
+      res.each { |v| @accounts[v['name']] = v['id'] }
+      res
     end
 
-    def list_records(email)
-      response = get('/records', additional_headers: { 'X-User' => email })
-      ap response
+    def list_currencies
+      res = get('/currencies')
+      res.each { |v| @currencies[v['code']] = v['id'] }
+      res
+    end
+
+    def list_records
+      get('/records')
+    end
+
+    def list_categories
+      res = get('/categories')
+      res.each { |v| @categories[v['name']] = v['id'] }
+      res
+    end
+
+    def create_record(options={})
+      required_params = %i(category_name account_name amount date currency)
+      raise MissingParams unless required_params.all? { |p| options[p] }
+      account_id = @accounts[options[:account_name]]
+      raise AccountNotFound if account_id.nil?
+      currency_id = @accounts[options[:currency]]
+      raise UnknownCurrency if currency_id.nil?
+      category_name = options[:category_name]
+      category_id = @categories[category_name] || create_category(category_name)
+      values = [
+          {
+              'categoryId': category_id,
+              'accountId': account_id,
+              'currencyId': currency_id,
+              'amount': options[:amount],
+              'paymentType': options[:payment_type] || 'credit',
+              'date': options[:date],
+              'note': options[:note] || '',
+          }
+      ]
+      post('/records-bulk', body: values.to_json)
+    end
+
+    def create_category(name, color=nil)
+      values = {
+          name: name,
+          color: color || '#ffffff',
+          icon: 0,
+          defaultType: 'expense',
+          position: 1000
+        }.to_json
+      response = post('/category', body: values)
+      @categories[name] = response['id']
+      response['id']
     end
 
     private
 
     def get(url, options={})
       @headers.merge!(options[:additional_headers]) if options[:additional_headers]
-      self.class.get(url, headers: @headers)
+      res = self.class.get(url, headers: @headers)
+      raise InvalidResponse unless res.code == 200
+      res
+    end
+
+    def post(url, options={})
+      @headers.merge!(options[:additional_headers]) if options[:additional_headers]
+      res = self.class.post(url, headers: @headers, body: options[:body])
+      raise InvalidResponse unless res.code == 201
+      res
     end
   end
 end
--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,3 +1,7 @@
 module Budgetbakers
   class TokenNotFoundException < StandardError; end
+  class MissingParams < StandardError; end
+  class InvalidResponse < StandardError; end
+  class AccountNotFound < StandardError; end
+  class UnknownCurrency < StandardError; end
 end
